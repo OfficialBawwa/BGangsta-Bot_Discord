ddad91c6ef606d1a8d67529f07106d7f199ab368
Bawwa Official <78470759+OfficialBawwa@users.noreply.github.com>
2021-11-09 20:12:43 +0530

    Update oldhelp.js

--- commands/Help/oldhelp.js
+++ commands/Help/oldhelp.js
@@ -22,7 +22,7 @@ module.exports = {
         "`addrole` `auditlog` `ban` `dm` `embed` `hackban` `kick` `lock` `nuke` `removerole` `addemoji` `unban` `unlock`"
       )
       .addField(
-        " :ChatBot: **CHATBOT**",
+        "<:ChatBot:881128707153100800> **CHATBOT**",
         "`chatbot-disable` `chatbot-set`"
       )
       .addField(
